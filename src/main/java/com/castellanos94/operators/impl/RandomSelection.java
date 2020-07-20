59b0ef324ea4ddabdcd57836b6a32429f906ad87
aca810@gmail.com <aca810@gmail.com>
2020-07-20 02:10:17 -0500

    TournamentSelection

--- src/main/java/com/castellanos94/operators/impl/RandomSelection.java
+++ src/main/java/com/castellanos94/operators/impl/RandomSelection.java
@@ -6,15 +6,18 @@ import java.util.Collections;
 import com.castellanos94.operators.SelectionOperator;
 import com.castellanos94.solutions.Solution;
 
-public class RandomSelection extends SelectionOperator {
+public class RandomSelection implements SelectionOperator {
+    private ArrayList<Solution> parents;
     private int size;
-    public RandomSelection(int size){
+
+    public RandomSelection(int size) {
         this.size = size;
-        this.parents = new ArrayList<>();
     }
+
     @Override
     public void execute(final ArrayList<Solution> solutions) {
         Collections.shuffle(solutions);
+        this.parents = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             parents.add(solutions.get(i));
         }
@@ -23,6 +26,11 @@ public class RandomSelection extends SelectionOperator {
     @Override
     public String toString() {
         return "RandomSelection [size=" + size + "]";
+    }
+
+    @Override
+    public ArrayList<Solution> getParents() {
+        return parents;
     }
 
 }
---
+++ src/main/java/com/castellanos94/operators/impl/TournamentSelection.java
@@ -0,0 +1,48 @@
+package com.castellanos94.operators.impl;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import com.castellanos94.operators.SelectionOperator;
+import com.castellanos94.solutions.Solution;
+import com.castellanos94.utils.Tools;
+
+public class TournamentSelection implements SelectionOperator {
+    private int n_offsrping;
+    private ArrayList<Solution> parents;
+    private Comparator<Solution> comparator;
+
+    public TournamentSelection(int n_offsrping, Comparator<Solution> comparator) {
+        this.n_offsrping = n_offsrping;
+        this.comparator = comparator;
+    }
+
+    @Override
+    public void execute(final ArrayList<Solution> solutions) {
+        parents = new ArrayList<>();
+        for (int i = 0; i < n_offsrping; i++) {
+            int pos_a = Tools.getRandom().nextInt(solutions.size());
+            int pos_b;
+            do {
+                pos_b = Tools.getRandom().nextInt(solutions.size());
+            } while (pos_b == pos_a);
+            int val = comparator.compare(solutions.get(pos_a), solutions.get(pos_b));
+            if (val == 1) {
+                parents.add(solutions.get(pos_a));
+            } else if (val == -1) {
+                parents.add(solutions.get(pos_b));
+            } else {
+                parents.add(solutions.get(pos_a));
+            }
+
+        }
+
+    }
+
+    @Override
+    public ArrayList<Solution> getParents() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
