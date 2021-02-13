44913b14b780223f03953b06f3ae02d8e9792653
castellanos94 <aca810@gmail>
2021-02-13 10:47:03 -0600

    NSGA3 experimentation parallel

--- src/main/java/com/castellanos94/experimentation/NSGA3WPExperimentation.java
+++ src/main/java/com/castellanos94/experimentation/NSGA3WPExperimentation.java
@@ -1,6 +1,7 @@
 package com.castellanos94.experimentation;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -37,21 +38,33 @@ import org.apache.logging.log4j.Logger;
  */
 public class NSGA3WPExperimentation {
     private static final Logger logger = LogManager.getLogger(NSGA3WPExperimentation.class);
-    private static final int CLASSIFY_EVERY_ITERATION = 1; // Classification F0 each
+    private static final int CLASSIFY_EVERY_ITERATION = 0; // Classification F0 each
     private static final int ELEMENTS_TO_REPLACE = 0; // 5 % of population
     private static final int numberOfObjectives = 10;
     static final String DIRECTORY = "experiments" + File.separator + numberOfObjectives + File.separator + "NSGA3"
             + File.separator + "C" + CLASSIFY_EVERY_ITERATION + "R" + ELEMENTS_TO_REPLACE;
-    static final int EXPERIMENT = 50;
+    static final int EXPERIMENT = 31;
 
     public static void main(String[] args) throws CloneNotSupportedException, IOException {
         new File(DIRECTORY).mkdirs();
-        for (int p = 1; p <= 7; p++) {
+        int[] problems = { 1, 2, 3, 4, 5, 6, 7 };
+        ArrayList<Integer> problelmArrayList = new ArrayList<>();
+        for (int integer : problems) {
+            problelmArrayList.add(integer);
+        }
+        problelmArrayList.stream().parallel().forEach(p -> {
+
+            // for (int p = 1; p <= 7; p++) {
 
             Tools.setSeed(1L);
             logger.info("Experimentation: DTLZ with preferences");
             String path = "src/main/resources/DTLZ_INSTANCES/" + numberOfObjectives + "/DTLZ" + p + "_Instance.txt";
-            DTLZ_Instance instance = (DTLZ_Instance) new DTLZ_Instance(path).loadInstance();
+            DTLZ_Instance instance = null;
+            try {
+                instance = (DTLZ_Instance) new DTLZ_Instance(path).loadInstance();
+            } catch (FileNotFoundException e3) {
+                e3.printStackTrace();
+            }
             logger.info(instance);
 
             NSGA_III_WP<DoubleSolution> algorithm = dtlzTestSuite(p, instance);
@@ -89,14 +102,22 @@ public class NSGA3WPExperimentation {
                 // "_" + i, algorithm.getSolutions());
                 bag.addAll(algorithm.getSolutions());
             }
-            reportResume(subDir);
-
-            logger.info("Resume " + problem.getName());
-            logger.info("Total time: " + averageTime);
-            logger.info("Average time : " + (double) averageTime / EXPERIMENT + " ms.");
-            logger.info("Solutions in the bag: " + bag.size());
-            Solution.writSolutionsToFile(DIRECTORY + File.separator + subDir + File.separator + "nsga_iii_bag_"
-                    + problem.getName() + "_F0_" + problem.getNumberOfObjectives(), new ArrayList<>(bag));
+            try {
+                reportResume(subDir);
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+            String str = "Resume " + problem.getName();
+            str += "\n" + "Total time: " + averageTime;
+            str += "\n" + "Average time : " + (double) averageTime / EXPERIMENT + " ms.";
+            str += "\n" + "Solutions in the bag: " + bag.size();
+            logger.info(str);
+            try {
+                Solution.writSolutionsToFile(DIRECTORY + File.separator + subDir + File.separator + "nsga_iii_bag_"
+                        + problem.getName() + "_F0_" + problem.getNumberOfObjectives(), new ArrayList<>(bag));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
             Ranking<DoubleSolution> compartor = new DominanceComparator<>();
             compartor.computeRanking(bag);
 
@@ -139,15 +160,19 @@ public class NSGA3WPExperimentation {
             if (front.isEmpty() && !hd.isEmpty()) {
                 front.addAll(hd);
             }
-            logger.info(String.format("HSat : %3d, Sat : %3d, Dis : %3d, HDis : %3d", hs.size(), s.size(), d.size(),
-                    hd.size()));
-            logger.info("Front 0: " + front.size());
+            logger.info(String.format("%s -> HSat : %3d, Sat : %3d, Dis : %3d, HDis : %3d", problem.getName(),
+                    hs.size(), s.size(), d.size(), hd.size()));
+            logger.info(problem.getName()+" -> Front 0: " + front.size());
 
             ArrayList<String> strings = new ArrayList<>();
             for (DoubleSolution solution : front)
                 strings.add(solution.toString());
 
-            Files.write(f.toPath(), strings, Charset.defaultCharset());
+            try {
+                Files.write(f.toPath(), strings, Charset.defaultCharset());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             if (problem.getNumberOfObjectives() == 3) {
                 Plotter plotter = new Scatter3D<DoubleSolution>(front, DIRECTORY + File.separator + subDir
                         + File.separator + "Class_F0" + problem.getName() + "_nsga3_WP");
@@ -155,18 +180,17 @@ public class NSGA3WPExperimentation {
                 // new Scatter3D(problem.getParetoOptimal3Obj(), directory + File.separator +
                 // problem.getName()).plot();
             } else {
-                Table table = Table.create(problem.getName() + "_F0_WP_" + problem.getNumberOfObjectives());
-                for (int j = 0; j < problem.getNumberOfObjectives(); j++) {
-                    DoubleColumn column = DoubleColumn.create("objective_" + j);
-                    for (int k = 0; k < front.size(); k++) {
-                        column.append(front.get(k).getObjective(j).doubleValue());
-                    }
-                    table.addColumns(column);
-                }
-                logger.info(table.summary());
+                /*
+                 * Table table = Table.create(problem.getName() + "_F0_WP_" +
+                 * problem.getNumberOfObjectives()); for (int j = 0; j <
+                 * problem.getNumberOfObjectives(); j++) { DoubleColumn column =
+                 * DoubleColumn.create("objective_" + j); for (int k = 0; k < front.size(); k++)
+                 * { column.append(front.get(k).getObjective(j).doubleValue()); }
+                 * table.addColumns(column); } logger.info(table.summary());
+                 */
             }
             logger.info("End Experimentation.");
-        }
+        });
     }
 
     @SuppressWarnings("unchecked")
@@ -174,7 +198,7 @@ public class NSGA3WPExperimentation {
 
         HashMap<String, Object> options = setup(instance.getNumObjectives());
         DTLZPreferences problem = null;
-        int maxIterations = 4000;
+        int maxIterations = 1000;
         int numberOfObjectives = instance.getNumObjectives();
         switch (p) {
             case 1:
