3839da8006751e2c72f3f650e7d8e424e932d9d8
castellanos94 <aca810@gmail>
2021-05-18 10:24:11 -0500

    Electri-Tri Preference Model

---
+++ src/main/java/com/castellanos94/preferences/impl/ELETRECT_TRI.java
@@ -0,0 +1,19 @@
+package com.castellanos94.preferences.impl;
+
+import com.castellanos94.preferences.Classifier;
+import com.castellanos94.solutions.Solution;
+
+public class ELETRECT_TRI<S extends Solution<?>> extends Classifier<S> {
+    
+    @Override
+    public void classify(S x) throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getAttributeKey() {
+        return "eletre-tri-key";
+    }
+
+}
