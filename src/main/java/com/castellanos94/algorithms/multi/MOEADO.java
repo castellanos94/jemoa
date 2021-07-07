6ce48f7f447ffa01299246df974083821bad536b
castellanos94 <aca810@gmail>
2021-07-07 09:50:52 -0500

    back

--- src/main/java/com/castellanos94/algorithms/multi/MOEADO.java
+++ src/main/java/com/castellanos94/algorithms/multi/MOEADO.java
@@ -1,7 +1,6 @@
 package com.castellanos94.algorithms.multi;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import com.castellanos94.components.impl.DominanceComparator;
 import com.castellanos94.datatype.Data;
@@ -12,10 +11,6 @@ import com.castellanos94.preferences.impl.IntervalOutrankingRelations;
 import com.castellanos94.preferences.impl.OutrankingModel;
 import com.castellanos94.problems.Problem;
 import com.castellanos94.solutions.Solution;
-import com.castellanos94.utils.Distance;
-import com.castellanos94.utils.Tools;
-
-import org.apache.commons.lang3.tuple.ImmutablePair;
 
 /**
  * E. Fernández, N. Rangel-Valdez, L. Cruz-Reyes, C. Gomez-Santillan, and C. A.
@@ -59,82 +54,7 @@ public class MOEADO<S extends Solution<?>> extends MOEAD<S> {
 
     }
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public void execute() {
-        this.init_time = System.currentTimeMillis();
-        // Step 1: initialization
-        this.solutions = new ArrayList<>();
-
-        b = new int[N][T];
-        // Step 1.2: compute eculidean distance between any two weight vectors and then
-        // work out the T closest weight vectors to each wegith vector
-        for (int i = 0; i < b.length; i++) {
-            List<Data> x = lambda.get(i);
-            ArrayList<ImmutablePair<Data, Integer>> distance_ = new ArrayList<>();
-            for (int j = 0; j < b.length; j++) {
-                List<Data> y = lambda.get(j);
-                if (i != j) {
-                    distance_.add(new ImmutablePair<Data, Integer>(Distance.euclideanDistance(x, y), j));
-                } else {
-                    distance_.add(new ImmutablePair<Data, Integer>(Data.getZeroByType(x.get(0)), i));
-                }
-            }
-            distance_.sort((a, b) -> a.left.compareTo(b.left));
-            for (int j = 0; j < T; j++) {
-                b[i][j] = distance_.get(j).getRight();
-            }
-        }
-        // Step 1.3: generate an initial population x_1 to x_N
-        ArrayList<S> FV = initPopulation();
-
-        this.dominanceComparator.computeRanking(FV);
-        for (S s : this.dominanceComparator.getSubFront(0)) {
-            this.solutions.add((S) s.copy());
-        }
-        // Step 1.4: initialize z
-        this.idealPoint = new ArrayList<>(problem.getNumberOfObjectives());
-        Data ref = FV.get(0).getObjective(0);
-        for (int i = 0; i < problem.getNumberOfObjectives(); i++) {
-            idealPoint.add(Data.initByRefType(ref,
-                    (problem.getObjectives_type()[i] == Problem.MAXIMIZATION) ? -Double.MAX_VALUE : Double.MAX_VALUE));
-        }
-        updateUtopianPoint(FV);
-        // Stopping criteria
-        while (isStoppingCriteriaReached()) {
-            for (int i = 0; i < N; i++) {
-                // Step 2.1 & 2.2: reproduction & impovement
-                ArrayList<S> parents = new ArrayList<>(2);
-                int k = Tools.getRandom().nextInt(T), l = Tools.getRandom().nextInt(T);
-                parents.add(FV.get(b[i][k]));
-                parents.add(FV.get(b[i][l]));
-                ArrayList<S> child = reproduction(parents);
-                // Step 2.3: update of z
-                updateUtopianPoint(child);
-                // Step 2.4: update of neighboring solutions
-                updateNeighboring(i, FV, child);
-                // Step 2.5: update of EP
-                this.solutions = replacement(solutions, child);
-            }
-            updateProgress();
-        }
-        computeTime = System.currentTimeMillis() - init_time;
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    protected ArrayList<S> replacement(ArrayList<S> population, ArrayList<S> offspring) {
-        ArrayList<S> tmp = new ArrayList<>(population);
-        offspring.forEach(s -> {
-            if (!tmp.contains(s)) {
-                tmp.add(s);
-            }
-        });
-        this.dominanceComparator.computeRanking(tmp);
-        ArrayList<S> res = new ArrayList<>();
-        this.dominanceComparator.getSubFront(0).forEach(s -> res.add((S) s.copy()));
-        return res;
-    }
+    
 
     @Override
     @SuppressWarnings("unchecked")
